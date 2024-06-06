22c2477c426b9eec83081f2be2c1c2ab4df35e92
leoprz606@gmail.com <leoprz606@gmail.com>
2024-06-06 22:55:21 +0000

    working. cgroup must not be disabled

---
+++ install-ansible.sh
@@ -0,0 +1,5 @@
+pip3 install ansible
+# Successfully installed MarkupSafe-2.1.5 PyYAML-6.0.1 ansible-4.10.0 ansible-core-2.11.12 cffi-1.15.1 cryptography-42.0.8 jinja2-3.1.4 packaging-24.0 pycparser-2.21 resolvelib-0.5.4
+
+export ANSIBLE_BECOME=true
+
